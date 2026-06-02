58439e40e4b960e86eca3a44a7425c32eb0d5708
Bruce <30441277+leurmxis@users.noreply.github.com>
2026-06-03 05:36:20 +0800

    Update data.py

--- data.py
+++ data.py
@@ -22,8 +22,8 @@ if sum(tradeday['trade_date'] == today):
     start_date = today
     end_date = today
 
-    start_date = '2026-06-01'
-    end_date = '2026-06-02'
+    # start_date = '2026-06-01'
+    # end_date = '2026-06-02'
     
     for row in code_df.itertuples(index=False):
         code = row[0]
